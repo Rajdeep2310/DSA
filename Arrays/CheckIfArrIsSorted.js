a6a25a4cc137f59f755345500352ddc0e97cfa50
Raj Deep <rajdeep@Rajs-Laptop.local>
2025-03-02 22:34:50 +0530

    :truck: check if array is sorted or not

---
+++ Arrays/CheckIfArrIsSorted.js
@@ -0,0 +1,13 @@
+// let arr2 = [ 23,1,3,24,52,2,0,0,221]
+let arr = [1,2,3,4,5,6,7,3]
+function sort(arr){
+    let orgArr = [...arr]
+    let sortedArr = arr.sort((a,b) => (a - b ))
+    if(JSON.stringify(orgArr) === JSON.stringify(sortedArr)){
+        console.log("true")
+    }else{
+        console.log("false")
+    }
+}
+
+sort(arr);
