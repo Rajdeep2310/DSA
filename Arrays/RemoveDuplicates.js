48a2886f30f4a08fe2ef8fa63c41e2b624f55f2a
Raj Deep <rajdeep@Rajs-Laptop.local>
2025-03-02 23:22:19 +0530

    :truck: Remove Duplicates

---
+++ Arrays/RemoveDuplicates.js
@@ -0,0 +1,15 @@
+// let arr2 = [ 23,1,3,24,52,2,0,0,221]
+let arr = [1,2,3,4,4,5,1,5,6,7,0]
+function remove(arr){
+    for(let i = 0 ; i <= arr.length-1 ; i++){
+        for(let j= i+1 ; j < arr.length; j++){
+            if(arr[i] === arr[j]){
+                arr.splice(j,1);
+            }
+        }
+    }
+    console.log(arr)
+}
+
+
+remove(arr)
