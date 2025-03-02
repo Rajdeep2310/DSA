a488631519300a55cd329d135221dbd16e9dfd20
Raj Deep <rajdeep@Rajs-Laptop.local>
2025-03-02 23:02:40 +0530

    :truck: check if array is sorted or not

--- Arrays/CheckIfArrIsSorted.js
+++ Arrays/CheckIfArrIsSorted.js
@@ -11,3 +11,22 @@ function sort(arr){
 }
 
 sort(arr);
+
+
+
+// Second Method 
+
+// let arr2 = [ 23,1,3,24,52,2,0,0,221]
+let arr = [1,2,3,4,5,6,7,0]
+
+function sort(arr){
+    for(let i = 0 ; i <= arr.length-1 ; i++){
+        if(arr[i] > arr[i+1]){
+            console.log(false)
+            return
+        }
+    }
+    console.log(true)
+}
+
+sort(arr);
